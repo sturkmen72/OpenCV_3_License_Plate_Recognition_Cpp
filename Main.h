f41f88e521d0b5ccec14436cee93400001427ec3
Suleyman TURKMEN <sturkmen@hotmail.com>
2022-02-01 01:05:27 +0300

    Update Main.h

--- Main.h
+++ Main.h
@@ -3,9 +3,9 @@
 #ifndef MY_MAIN         // used MY_MAIN for this include guard rather than MAIN just in case some compilers or environments #define MAIN already
 #define MY_MAIN
 
-#include<opencv2/core/core.hpp>
-#include<opencv2/highgui/highgui.hpp>
-#include<opencv2/imgproc/imgproc.hpp>
+#include<opencv2/core.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
 
 #include "DetectPlates.h"
 #include "PossiblePlate.h"
@@ -24,7 +24,7 @@ const cv::Scalar SCALAR_GREEN = cv::Scalar(0.0, 255.0, 0.0);
 const cv::Scalar SCALAR_RED = cv::Scalar(0.0, 0.0, 255.0);
 
 // function prototypes ////////////////////////////////////////////////////////////////////////////
-int main(void);
+int main(int argc, char** argv);
 void drawRedRectangleAroundPlate(cv::Mat &imgOriginalScene, PossiblePlate &licPlate);
 void writeLicensePlateCharsOnImage(cv::Mat &imgOriginalScene, PossiblePlate &licPlate);
 
